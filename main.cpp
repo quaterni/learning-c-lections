ad8f841c17206860d570b7381170e29de9d84a5a
igor <igorzhadko.w@gmail.com>
2020-09-15 22:40:05 +0500

    Изменения комманд Добавил вывод доступных комманд на экран Добавил команду вывод лекций

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ const int countOfLections = 2;
 
 void printLections(std::string lections[])
 {
+	std::cout << "Доступные лекции:" << std::endl;
 	for (int i = 0; i < countOfLections; i++) {
 		std::cout << lections[i] << std::endl;
 	}
@@ -16,11 +17,11 @@ void main()
 	setlocale(LC_ALL, "Rus");
 	std::string lections[countOfLections] =  { "Лекция 1: Типы данных", "Лекция 2: Указатели и константы"};
 
-	std::cout << "Доступные лекции:"<<std::endl;
 	printLections(lections);
 	char command[15];
 
 	while(true) {
+		std::cout << "Доступные комманды: lections, lection number, exit" << std::endl;
 		std::cout << "Введите команду" << std::endl;
 		gets_s(command);
 		std::cout << std::endl << std::endl;
@@ -35,6 +36,9 @@ void main()
 		}
 		else if (strcmp("exit", command) == 0) {
 			break;
+		}
+		else if (strcmp("lections", command) == 0) {
+			printLections(lections);
 		}
 		else {
 			std::cout << "Неверная команда" << std::endl;
